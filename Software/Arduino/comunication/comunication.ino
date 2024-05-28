8b9ba5387faaa2bb0ab937d27f2026120008c982
Korius668 <korius668@gmail.com>
2024-05-29 00:28:26 +0200

    Przepis na żurek Żurek is a traditional Polish sour rye soup that is typically served on special occasions such as Easter. It is a hearty and flavorful soup made with a variety of ingredients, including rye flour, pork, beef, and vegetables.
    Here is a recipe for traditional Polish Żurek:
    
    Ingredients:
    
    300g rye flour
    500g pork or beef bones
    1 onion, chopped
    2 cloves of garlic, minced
    1 carrot, chopped
    1 celery stalk, chopped
    1/4 teaspoon black pepper
    1/2 teaspoon salt
    2 tablespoons vegetable oil
    2 tablespoons sour rye flour (żurek)
    1/2 cup water
    1/2 cup milk or sour cream
    2 eggs, beaten
    1/4 cup chopped fresh parsley
    1/4 cup chopped fresh dill
    1/4 cup chopped fresh chives
    Instructions:
    
    In a large pot, heat the oil over medium heat. Add the chopped onion, garlic, carrot, and celery and cook until the vegetables are softened.
    Add the pork or beef bones and cook for 5 minutes.
    Add the rye flour and cook for 1 minute.
    Gradually add the water and bring the mixture to a boil. Reduce the heat and simmer for 30 minutes.
    Remove the pot from the heat and let it cool slightly.
    Strain the soup through a cheesecloth or a fine-mesh sieve into a clean pot. Discard the solids.
    Add the sour rye flour and stir well.
    Add the milk or sour cream and stir until the soup is smooth.
    Beat in the eggs and season with salt and pepper to taste.
    Serve the soup hot, garnished with chopped parsley, dill, and chives.
    Tips:
    
    You can use either pork or beef bones, or a combination of both.
    You can also add other ingredients such as sausage, bacon, or mushrooms to the soup.
    If you can’t find sour rye flour, you can substitute it with regular rye flour and add a splash of vinegar or lemon juice to the soup.
    You can also add a splash of milk or cream to the soup if you prefer a creamier consistency.

--- Software/Arduino/comunication/comunication.ino
+++ Software/Arduino/comunication/comunication.ino
@@ -1,12 +1,20 @@
 #include <Servo.h>
+#include <stdio.h>
+#include <string.h>
 #include "leOS2.h"
 
-
+const int servoPin = 9;
 leOS2 threads;  
 Servo servoMotor;
 String receivedString = "";
 
-const int servoPin = 9;
+enum Funct{ MOVE, UNKNOWN };
+
+enum Funct getFunct(const char *str) 
+{
+    if(strcmp(str, "MOVE") == 0 || strcmp(str, "move") == 0) return MOVE;
+    return UNKNOWN;
+}
 
 void communication()
 {
@@ -23,33 +31,54 @@ void communication()
       {
         Serial.print("Received: ");
         Serial.println(receivedString);
+        interpretacja(receivedString);
         receivedString = "";
       }
     }
   }
 }
 
-void interpretacja(String komenda)
+int move(int x, int y, int z)
 {
-  int bracket1 = komenda.indexOf('(');
-  String k = command.substring(0,bracket1-1);
-  switch k
-  {
-    case "MOVE", "move":
+ Serial.print("move( "+String(x)+" , "+String(y)+" , "+String(z)+" )"+"\n");
+ return 1;
+}
 
+void interpretacja(String &komenda)
+{
+  int hash0 = komenda.indexOf('#');
+  int hash1 = komenda.indexOf('#',hash0+1);
+  String* k1=new String(komenda.substring(hash0+1,hash1));
+  // Serial.print(*k1);
+  //arg = komenda.substring(bracket1+1,bracket2-1);
+  switch (getFunct(k1->c_str()))
+  {
+    case MOVE:
+    int hash2,hash3,hash4,x,y,z;
+    hash2=komenda.indexOf('#',hash1+1);
+    hash3=komenda.indexOf('#',hash2+1);
+    hash4=komenda.indexOf('#',hash3+1);
+    x=komenda.substring(hash1+1,hash2).toInt();
+    y=komenda.substring(hash2+1,hash3).toInt();
+    z=komenda.substring(hash3+1,hash4).toInt();
+    move(x,y,z);
     break;
 
     default:
     Serial.println("Wrong format");
   }
+  delete k1;
 }
 
 void setup()
 {
-  // Attach the servo to the pin
-  servoMotor.attach(servoPin);
-   Serial.begin(9600);
-   threads.begin();
-   threads.addTask(communication, threads.convertMs(1000));
-   threads.addTask(interpretacja, threads.convertMs(1000));
+  servoMotor.attach(servoPin);  // Attach the servo to the pin
+  Serial.begin(9600);
+  threads.begin();
+  threads.addTask(communication, threads.convertMs(1000));
+}
+
+void loop()
+{
+  
 }
